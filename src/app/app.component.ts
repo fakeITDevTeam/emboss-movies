4302707d45d1afa15da089da9781969c7c809abb
fakeITDevTeam <dev5544455@gmail.com>
2023-09-30 19:51:37 +0700

    add navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,40 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emboss-movies';
-
-  today = new Date;
   
-  imageUrl = 'https://dotnethow.net/images/movies/movie-1.jpeg';
-  imageWidth = 150;
-  imageHeight = 200;
-
-  price = 10;
-
-  sellingPrice = 5000000000*12;
-
-  attributeValue = "movies-app-test-value";
-
-  isSampleApplied = true;
-
-  multipleClasses = 'sample-bgcolor sample-color';
-
-  multipleStyles = 'background-color: blueviolet; color: white'; //style
-
-  bgcolor = 'blueviolet';
-
-  forecolor = "white";
-
-  timeNow = new Date();
-
-  setTimeToNow() {
-    console.log(new Date());
-    this.timeNow = new Date();
-  }
-
-  textColor = '';
-  setTextColor(_color: string) {
-    console.log(_color);
-    this.textColor = _color;
-  }
 }
