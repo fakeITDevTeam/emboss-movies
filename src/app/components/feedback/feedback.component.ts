ec39b595e0b4dedf85254b74a262ba93b9dfc6c6
fakeITDevTeam <dev5544455@gmail.com>
2024-01-23 12:30:37 +0700

    building reactive forms

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Feedback } from 'src/app/data/model/Feedback';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css']
 })
-export class FeedbackComponent {
+export class FeedbackComponent implements OnInit {
+  
+  // feedback: Feedback = new Feedback('','',8,'','');
+  feedbackForm!: FormGroup;
+    
+  constructor(private fb: FormBuilder) {
+        
+  }
 
+  ngOnInit(): void {
+    this.feedbackForm = this.fb.group({
+      emailAddress: [''],
+      phoneNumber: [''],
+      rate: [8],
+      feedbackTitle: [''],
+      feedback: ['']
+    })
+  }
+
+  submitFeedback() {
+    // console.log('Feedback from [Submit] - ', this.feedback)
+    console.log('Feedback from [Submit] - ', this.feedbackForm.value)
+  }
 }
